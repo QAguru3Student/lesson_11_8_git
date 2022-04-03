2028233f95450304933323db7c2c4b64cb03f4af
Babushkin Anton <napishi_antonu@mail.ru>
2022-04-03 17:20:05 +0300

    added assertFalse axemple

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleTest {
     @Test
     void assertTrueTest(){
         assertTrue(3>2);
+    }
+@Test
+    void assertFalseTest(){
+        assertFalse(3<2);
     }
 }
